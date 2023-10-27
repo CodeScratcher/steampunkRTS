2b6d2f83ddbb75c2f84d235e3aca4b7e93fe0f23
CodeScratcher <arialexanderfisher@outlook.com>
2023-10-27 13:10:15 -0400

    Added factory class

---
+++ steampunkRTS/Factory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace steampunkRTS
+{
+    internal class Factory : IRenderableEntity
+    {
+        public void render(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(KeyboardState kstate, MouseState mstate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
