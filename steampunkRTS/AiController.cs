da8a49c8073f58ec10b65a482382636e8bcba0c0
CodeScratcher <arialexanderfisher@outlook.com>
2023-12-07 13:44:12 -0500

    Sending commands to enemy troops as AI

--- steampunkRTS/AiController.cs
+++ steampunkRTS/AiController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace steampunkRTS
 {
-    internal class AiController
+    internal class AiController : Controller
     {
+        int money = 100;
+
+        public List<IEntity> entities;
+        public void commandEntities(KeyboardState keyboardState, MouseState mouseState)
+        {
+            foreach (IEntity entity in entities)
+            {
+                EnemyTroop troop = entity as EnemyTroop;
+                if (troop != null)
+                {
+                    troop.receiveCommand(new MoveCommand(0, 0));
+                }
+            }
+        }
+
+        public int getMoney()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setMoney(int x)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
