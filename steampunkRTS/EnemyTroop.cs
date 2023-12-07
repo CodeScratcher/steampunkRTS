0966807ac316ea4cd85cc10eb1365ed9efca5b59
CodeScratcher <arialexanderfisher@outlook.com>
2023-12-07 13:31:58 -0500

    Set up boilerplate for AI

---
+++ steampunkRTS/EnemyTroop.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace steampunkRTS
+{
+    internal class EnemyTroop : ICommandable, IRenderableEntity,
+    {
+        public Rectangle getBoundingBox()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<(string, int)> getGuiCommands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void receiveCommand(Command command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void render(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(KeyboardState kstate, MouseState mstate, GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
